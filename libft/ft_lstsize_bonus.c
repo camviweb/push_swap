2d079a9395ff126df82a4b078cea60e655a27c7a
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-04-07 15:04:11 +0200

    Add files via upload

--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 11:44:46 by canguyen          #+#    #+#             */
+/*   Updated: 2025/04/07 11:44:49 by canguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_lstsize(t_list *lst)
 {
-	t_list *tmp;
-	unsigned int i;
+	t_list			*tmp;
+	unsigned int	i;
 
 	tmp = lst;
 	i = 0;
--- libft/libft.h
+++ libft/libft.h
@@ -17,6 +17,12 @@
 # include <stdlib.h> // exit, free, malloc
 # include <unistd.h> // write, read
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
@@ -65,19 +71,15 @@ int					do_format(const char *s, va_list args, int i);
 int					handle_pointer_null(unsigned long long adr);
 
 // others
-long	ft_atol(const char *str);
+long				ft_atol(const char *str);
 
 //Partie bonus
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+
 // void	ft_lstdelone(t_list *lst, void (*del)(void *));
 // void	ft_lstclear(t_list **lst, void (*del)(void *));
 // void	ft_lstiter(t_list *lst, void (*f)(void *));
